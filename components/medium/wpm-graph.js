5a6a8f70ca6de914bb34d2a2829ce7ba62372d1c
THPS-Hendrick <tom@tomhendrick.com>
2026-06-25 15:03:45 +0930

    Update wpm-graph.js

--- components/medium/wpm-graph.js
+++ components/medium/wpm-graph.js
@@ -44,6 +44,12 @@ class ThpsWpmGraph extends HTMLElement {
 
         this.chartInstance = null;
         window.addEventListener('thps-dashboard-update', (e) => this.update(e.detail));
+        
+        // NEW: THE "WAKE-UP" CATCH-UP CHECK
+        if (window.thps_lastPayload) {
+            setTimeout(() => this.update(window.thps_lastPayload), 50);
+        }
+
     }
 
     update(data) {
