d5ee44900ec70a55eaa82eb017fd2aa6d79ad97f
THPS-Hendrick <tom@tomhendrick.com>
2026-07-09 16:34:53 +0930

    Create dummy-course.js

---
+++ courses/dummy-course.js
@@ -0,0 +1,24 @@
+{
+    "title": "Pitching Level 1",
+    "totalSteps": 3,
+    "modules": [
+        {
+            "step": 1,
+            "title": "1. Self-Assessment",
+            "type": "slider",
+            "instructions": "Adjust the sliders below to indicate your current comfort levels."
+        },
+        {
+            "step": 2,
+            "title": "2. Visual Capacity",
+            "type": "audio",
+            "instructions": "Listen to the prompt and select your visual capacity."
+        },
+        {
+            "step": 3,
+            "title": "3. The Final Mic-Check",
+            "type": "recording",
+            "instructions": "Goal: 60 Seconds. Pitch your idea."
+        }
+    ]
+}
